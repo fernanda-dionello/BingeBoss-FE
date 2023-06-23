706522f57a858e6e87823ed16cef7f06156776e6
fernanda-dionello <fernanda.dionello@gmail.com>
2023-06-23 20:08:18 -0300

    chore: change imports

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router} from "react-router-dom"
 import './App.css';
-import { NavTop } from "../src/app/components/NavTop/NavTop";
-import { AuthProvider } from '../src/app/context/AuthContext';
-import RouterWrapper from '../src/app/router/Router';
+import { NavTop } from "./components/NavTop/NavTop";
+import { AuthProvider } from './context/AuthContext';
+import RouterWrapper from './router/Router';
 import addNotification from 'react-push-notification';
 
 function App() {
   useEffect(() => {
     const clickTONotify = () => {
       addNotification({
-          title: "Test",
-          message: "testing",
+          title: "Welcome Back!",
+          message: "You are the BingeBoss here!",
           duration: 10000,
           native: true
       })
