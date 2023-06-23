0f9175576b257ff293ef36ff1a5045e5e8745956
fernanda-dionello <fernanda.dionello@gmail.com>
2023-06-23 19:30:55 -0300

    chore: update config files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./App.css";
-import { NavTop } from "./app/components/NavTop/NavTop";
-import { AuthProvider } from "./app/context/AuthContext";
-import RouterWrapper from "./app/router/Router";
-import "./messaging_init_in_sw";
+import React from 'react';
+import { BrowserRouter as Router} from "react-router-dom"
+import './App.css';
+import { NavTop } from "../src/app/components/NavTop/NavTop";
+import { AuthProvider } from '../src/app/context/AuthContext';
+import RouterWrapper from '../src/app/router/Router';
+import './messaging_init_in_sw';
 
-
-function App() {  
+function App() {
   return (
     <Router>
       <AuthProvider>
         <div className="App">
-          <NavTop className="navTop" />
+          <NavTop className="navTop"/>
           <RouterWrapper />
         </div>
       </AuthProvider>
     </Router>
+    
   );
 }
 
