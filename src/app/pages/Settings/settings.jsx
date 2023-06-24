51dcd031bf619e7c7a275b608bd9de26dff57df2
Fernanda Dionello <fernandadionello@Laptop-de-Fernanda.local>
2023-06-24 16:32:55 -0300

    feat: add spoiler protection switch button

--- src/app/pages/Settings/settings.jsx
+++ src/app/pages/Settings/settings.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './settings.css';
 
 export function Settings(){
   const [ updateAccountModal, setUpdateAccountModal ] = useState(false);
-
+  const [checked, setChecked] = useState(false);
 
     return (
         <div className="settings-container">
@@ -13,6 +14,18 @@ export function Settings(){
             <Button className="button settings-button" variant="primary" size="sm" onClick={() => setUpdateAccountModal(true)}>Logout</Button>
             <Button className="button settings-button" variant="primary" size="sm" onClick={() => setUpdateAccountModal(true)}>Update Profile</Button>
             <Button className="button settings-button" variant="primary" size="sm" onClick={() => setUpdateAccountModal(true)}>Delete Account</Button>
+            <div className="settings-spoiler-container">
+              <h4 className='settings-spoiler-text'>Spoiler Protection</h4>
+              <Form.Switch 
+                type="switch"
+                id="custom-switch"
+                className='spoiler-switch'
+                checked={checked}
+                onChange={(e) => {
+                  setChecked((checked) ? false : true);
+                }}
+              />
+            </div>
           </div>
           {/* <UpdateUser isOpen={updateAccountModal} onClose={() => setUpdateAccountModal(false)}/> */}
         </div> 
