0414a77a1576f56baec8ca33c498bb87b083e5ac
Fernanda Dionello <fernandadionello@Laptop-de-Fernanda.local>
2023-06-24 15:31:31 -0300

    feat: add settings menu buttons

--- src/app/pages/Settings/settings.jsx
+++ src/app/pages/Settings/settings.jsx
@@ -1,14 +1,20 @@
-import { useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import './settings.css';
 
 export function Settings(){
-    const { state } = useLocation();
+  const [ updateAccountModal, setUpdateAccountModal ] = useState(false);
 
 
     return (
-        <>
-          <div className="settings-container">
-          </div> 
-        </>
+        <div className="settings-container">
+          <div className='settings-menu'>
+            <h2 className='settings-text'>Configuration</h2>
+            <Button className="button settings-button" variant="primary" size="sm" onClick={() => setUpdateAccountModal(true)}>Logout</Button>
+            <Button className="button settings-button" variant="primary" size="sm" onClick={() => setUpdateAccountModal(true)}>Update Profile</Button>
+            <Button className="button settings-button" variant="primary" size="sm" onClick={() => setUpdateAccountModal(true)}>Delete Account</Button>
+          </div>
+          {/* <UpdateUser isOpen={updateAccountModal} onClose={() => setUpdateAccountModal(false)}/> */}
+        </div> 
     )
 }
